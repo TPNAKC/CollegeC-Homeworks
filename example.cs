ebd1965bafa6428b25ea20b246eb620e718ae251
TPNAKC <maksimfilkov951375@gmail.com>
2025-10-02 19:02:28 +0300

    Add files via upload

---
+++ example.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+double score = 15.7865784786568470548764678540765468785467;
+double min = 15.56786796087540687967097857869879548783465739356472;
+double result = score * min;
+Console.WriteLine(result);
+Console.ReadLine();
