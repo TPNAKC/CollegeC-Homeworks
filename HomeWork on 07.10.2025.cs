41a51ef664bdd9fc970c9510557673dc79d0be20
TPNAKC <maksimfilkov951375@gmail.com>
2025-10-07 15:10:02 +0300

    HomeWorkNumber3

---
+++ HomeWork on 07.10.2025.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Калькулятор Индекса Массы Тела (ИМТ) ---"); 
+Console.WriteLine("Введите ваш вес в килограммах:");
+string weightInput = Console.ReadLine();
+double weightResult = Convert.ToDouble(weightInput);
+Console.WriteLine("Введите ваш рост в метрах:"); // Дробные числа вводить ТОЛЬКО через запятую,иначе код не будет работать и выдаст ошибку.
+string heightInput = Console.ReadLine();
+double heightResults = Convert.ToDouble(heightInput);
+double quotient = weightResult / (heightResults * heightResults);
+Console.WriteLine("--- Ваш результат ---");
+Console.WriteLine($"При весе {weightResult} и росте {heightResults}, ваш ИМТ составляет: {quotient}");
+Console.ReadLine();
