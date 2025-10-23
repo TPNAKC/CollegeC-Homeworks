18d99ab8f13b576a1b24d44a9314a0ce1ff97113
TPNAKC <maksimfilkov951375@gmail.com>
2025-10-23 18:40:02 +0300

    Add files via upload

---
+++ HomeWork/HomeWork 24.10.2025.cs
@@ -0,0 +1,57 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Print("Введите первое число: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+        Print("Введите второе число: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+        Print("Введите символ операции: ");
+        char char1 = Convert.ToChar(Console.ReadLine());
+
+        if (char1 == '*')
+        {
+            Multiplication(number1, number2);
+        }
+        else if (char1 == '+')
+        {
+            Addition(number1, number2);
+        }
+        else if (char1 == '-')
+        {
+            Subtraction(number1, number2);
+        }
+        else if (char1 == '/')
+        {
+            Division(number1, number2);
+        }
+        Console.ReadLine();
+    }
+    public static void Print(string s)
+    {
+        Console.WriteLine(s);
+    }
+    public static void Multiplication(double number, double number2)
+    {
+        double opi = number * number2;
+        Console.WriteLine($"Результат: {opi} ");
+    }
+    public static void Addition(double number, double number2)
+    {
+        double opi = number + number2;
+        Console.WriteLine($"Результат: {opi} ");
+    }
+    public static void Subtraction(double number, double number2)
+    {
+        double opi = number - number2;
+        Console.WriteLine($"Результат: {opi} ");
+    }
+    public static void Division(double number, double number2)
+    {
+        double opi = number / number2;
+        Console.WriteLine($"Результат: {opi} ");
+    }
+
+
+
+}
