934b163e3d589a0b1b6ac28414d18df8888ece43
TPNAKC <maksimfilkov951375@gmail.com>
2025-10-29 22:33:50 +0300

    Add files via upload

---
+++ HomeWork/HomeWork26.10.2025.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Print("Введите число:");
+        int factorial = Convert.ToInt32(Console.ReadLine());
+        int number = Fact(factorial);
+        if (number >= 0)
+        {
+            Console.WriteLine(number);
+        }
+        else
+        {
+            Print("Ошибка");
+        }
+    }
+    public static int Fact(int n)
+    {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+    public static void Print(string n)
+    {
+        Console.WriteLine(n);
+    }
+}
