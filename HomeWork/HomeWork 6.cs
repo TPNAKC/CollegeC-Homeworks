22cd02da40ad3cadf94783a9cc9a5849f3705737
TPNAKC <maksimfilkov951375@gmail.com>
2025-11-05 17:39:26 +0300

    Add files via upload

---
+++ HomeWork/HomeWork 6.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Исходный массив:");
+        int[] array = {10,20,30,40,50};
+        for (int i = 0;i < 5; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine("\nПеревернутый массив:");
+        for(int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write($"{array[i]} ");
+        }
+
+    }
+}
