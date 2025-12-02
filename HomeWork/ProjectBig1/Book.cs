b9656c33a68e2f25621b523197433d9903ff1d27
TPNAKC <maksimfilkov951375@gmail.com>
2025-12-02 20:18:28 +0300

    Update Book.cs

--- HomeWork/ProjectBig1/Book.cs
+++ HomeWork/ProjectBig1/Book.cs
@@ -8,5 +8,10 @@ namespace ConsoleApp23
 {
     internal class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
     }
 }
+
